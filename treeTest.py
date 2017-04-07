26a03bc75f7b175071bf80789c3bd1d6e2295154
Francisco Favela <ffavela@gmail.com>
2017-04-07 16:30:44 +0200

    fooling with surfaces

--- treeTest.py
+++ treeTest.py
@@ -31,9 +31,9 @@ alphaCDict={"type":tP,"name":"4He","mass":m4He}
 # alphaEDict={"type":tP,"name":"4He","mass":m4He}
 
 #Defining the detectors
-d1Dict={"type":tD,"name":"d1","angles":[radians(35),radians(24)]}
-d2Dict={"type":tD,"name":"d2","angles":[radians(16),radians(56)]}
-d3Dict={"type":tD,"name":"d3","angles":[radians(179),radians(67)]}
+d1Dict={"type":tD,"name":"d1","angles":[radians(34),radians(6)]}
+d2Dict={"type":tD,"name":"d2","angles":[radians(23),radians(200)]}
+d3Dict={"type":tD,"name":"d3","angles":[radians(140),radians(20)]}
 # d4Dict={"type":tD,"name":"d4"}
 
 #Completing the dictionaries
@@ -83,6 +83,44 @@ printTree(initDict)
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
+plt.xlim(-15, 15)
+plt.ylim(-15, 15)
+# plt.zlim(-15, 15)
+
 plotAllLines(initDict,ax)
 ax.legend()
+
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+class Arrow3D(FancyArrowPatch):
+
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+
+
+firstVelMag=initDict["redVcm"]
+a = Arrow3D([0, 0], [0, 0], [0, firstVelMag], mutation_scale=20,
+            lw=1, arrowstyle="-|>", color="k")
+# a = Arrow3D([0,1],[0,1],[0,1], mutation_scale=20, lw=1, arrowstyle="-|>", color="k", linestyle="dashed")
+
+ax.add_artist(a)
+
+u = np.linspace(0, 2 * np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+
+# oxyVel=initDict["dictList"][0]["redVcm"]
+oxyVel=2.0
+x = oxyVel * np.outer(np.cos(u), np.sin(v))
+y = oxyVel * np.outer(np.sin(u), np.sin(v))
+z = oxyVel * np.outer(np.ones(np.size(u)), np.cos(v))+firstVelMag
+ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='#668800')
+
+
 plt.show()
